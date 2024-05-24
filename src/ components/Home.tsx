bf3cbb6621374423b31d76ca698a99b41c05a27c
João Victor <j_victorfernandes@hotmail.com>
2024-05-24 17:04:33 -0300

    feature: update home and tradutor

--- src/ components/Home.tsx
+++ src/ components/Home.tsx
@@ -1,22 +1,61 @@
 import React from "react";
 import styled from "styled-components";
 import { useTranslation } from "react-i18next";
+import { useTheme } from "../ThemeContext";
 
 const HomeContainer = styled.main`
+	display: flex;
+	justify-content: space-evenly;
+	align-items: start;
 	background: ${({ theme }) => theme.colors.body};
 	color: ${({ theme }) => theme.colors.text};
-	text-align: center;
 	padding: 2rem;
 	min-height: calc(100vh - 8rem);
+	margin: 0 2rem;
+	padding-top: 10rem;
+`;
+
+const CircleContainer = styled.div`
+	flex: 0 0 auto; /* Não se expande, tamanho automático */
+`;
+
+const Circle = styled.div`
+	width: 200px;
+	height: 200px;
+	border-radius: 50%;
+	background: ${({ theme }) => theme.colors.header};
+	overflow: hidden;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Description = styled.div`
+	max-width: 600px;
+`;
+
+const ExperienceSection = styled.section`
+	text-align: left;
+	margin-top: 2rem;
 `;
 
 const Home: React.FC = () => {
 	const { t } = useTranslation();
+	const { darkMode } = useTheme();
 
 	return (
 		<HomeContainer>
-			<h2>{t("welcome")}</h2>
-			<p>{t("description")}</p>
+			<CircleContainer>
+				<Circle>
+					<img src="caminho/para/imagem.jpg" alt="Foto" />
+				</Circle>
+			</CircleContainer>
+			<Description>
+				<h2>{t("experience")}</h2>
+				<ExperienceSection>
+					<h3>{t("position")}</h3>
+				</ExperienceSection>
+			</Description>
 		</HomeContainer>
 	);
 };
--- src/i18next.ts
+++ src/i18next.ts
@@ -2,30 +2,34 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-const tittle = "JOAODEV.COM";
+const title = "JOAODEV.COM";
 
 const resources = {
 	en: {
 		translation: {
-			title: tittle,
+			title,
 			welcome: "Welcome to my portfolio",
 			description:
 				"Here you will find information about my projects and skills.",
+			experience: "Experience",
+			position: "Position",
 		},
 	},
 	pt: {
 		translation: {
-			title: tittle,
+			title,
 			welcome: "Bem-vindo ao meu portfólio",
 			description:
 				"Aqui você encontrará informações sobre meus projetos e habilidades.",
+			experience: "Experiencia",
+			position: "Posição",
 		},
 	},
 };
 
 i18n.use(initReactI18next).init({
 	resources,
-	lng: "en", // default language
+	lng: "pt",
 	interpolation: {
 		escapeValue: false,
 	},
